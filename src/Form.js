2a690620a5572a75631c319079823e11774affb6
Heather Frantisak <heathernfran@gmail.com>
2017-10-31 13:57:00 -0700

    Get new todo in submitted form

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+    }
+  }
+
   addTodo(e) {
     e.preventDefault()
+    let newTodo = e.target.todo.value
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value})
   }
 
   render() {
     return (
-      <div>
-        <input type="text" />
-        <button onClick={(e) => this.addTodo(e)}>+</button>
-      </div>
+      <form onSubmit={(e) => this.addTodo(e)}>
+        <input type="text" name="todo" value={this.state.value} onChange={(e) => this.handleChange(e)} />
+        <button type="submit">+</button>
+      </form>
     )
   }
 }
