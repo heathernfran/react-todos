068d52f566b7d686349a131e1b50f042fb16796c
Heather Frantisak <heathernfran@gmail.com>
2017-11-01 09:53:45 -0700

    Remove constructor and handleChange, remove value from input field. Can be converted to stateless component.

--- src/Form.js
+++ src/Form.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: '',
-    }
-  }
-
   addTodo(e) {
     e.preventDefault()
     let newTodo = e.target.todo.value
     this.props.allTodos(newTodo)
   }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value})
-  }
-
   render() {
     return (
-      <form onSubmit={(e) => this.addTodo(e)}>
-        <input type="text" name="todo" value={this.state.value} onChange={(e) => this.handleChange(e)} />
+      <form onSubmit={e => this.addTodo(e)}>
+        <input type="text" name="todo" />
         <button type="submit">+</button>
       </form>
     )
