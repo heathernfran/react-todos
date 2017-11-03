31b8efd13f42a25eb17e5bb160faef248a0f385c
Heather Frantisak <heathernfran@gmail.com>
2017-11-03 14:41:24 -0700

    Replace Object.assign with spread operator

--- src/reducers.js
+++ src/reducers.js
@@ -4,34 +4,27 @@ import {
  } from './actions'
 
 const initialState = {
-  todos: []
+  todos: [{text: 'initial todo', isEditing: false}]
 }
 
 const todoApp = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO:
       console.log('add todo', state, action)
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos,
-          {
+      return {
+        ...state, todos: [
+          ...state.todos, {
             text: action.text,
             isEditing: false
-          }
-        ]
-      })
+          }]}
     case TOGGLE_EDIT:
       console.log('toggle edit', state, action)
-      return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              isEditing: !todo.isEditing
-            })
-          }
-          return todo
-        })
-      })
+      return {...state, todos: state.todos.map((todo, index) => {
+        if (index === action.index) {
+          return {...todo, isEditing: !todo.isEditing}
+        }
+        return todo
+      })}
     default:
       console.log(state, action)
       return state
