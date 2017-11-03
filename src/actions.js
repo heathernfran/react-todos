d153916da1305b9a3a3906241972ec22684e1107
Heather Frantisak <heathernfran@gmail.com>
2017-11-03 14:04:26 -0700

    Add reducers and actions for add todo and toggle edit

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,16 @@
 export const ADD_TODO = 'ADD_TODO'
+export const TOGGLE_EDIT = 'TOGGLE_EDIT'
 
-export function addTodo(text) {
+export const addTodo = (text) => {
   return {
     type: ADD_TODO,
     text
+  }
+}
+
+export const toggleEdit = (isEditing) => {
+  return {
+    type: TOGGLE_EDIT,
+    isEditing
   }
 }
--- src/reducers.js
+++ src/reducers.js
@@ -1,6 +1,41 @@
-function todoApp(state = [], action) {
-  console.log(state, action)
-  return state
+import {
+  ADD_TODO,
+  TOGGLE_EDIT
+ } from './actions'
+
+const initialState = {
+  todos: []
+}
+
+const todoApp = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      console.log('add todo', state, action)
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            isEditing: false
+          }
+        ]
+      })
+    case TOGGLE_EDIT:
+      console.log('toggle edit', state, action)
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              isEditing: !todo.isEditing
+            })
+          }
+          return todo
+        })
+      })
+    default:
+      console.log(state, action)
+      return state
+  }
 }
 
 export default todoApp
